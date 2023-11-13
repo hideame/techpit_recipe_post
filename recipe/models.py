338c6b8155c2d2ece226626b54825e69db18d247
hideame <hamezawa3@gmail.com>
2023-11-13 11:16:30 +0900

    一覧ページ作成

--- recipe/models.py
+++ recipe/models.py
@@ -2,15 +2,15 @@ from django.db import models
 
 
 class Recipe(models.Model):
+    title = models.CharField(verbose_name="タイトル", max_length=200)
+    content = models.TextField(verbose_name="内容")
+    description = models.TextField(blank=True, default="")
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
     class Meta:
         verbose_name = "レシピ"
         verbose_name_plural = "レシピ"
 
     def __str__(self):
         return self.title
-
-    title = models.CharField(verbose_name="タイトル", max_length=200)
-    content = models.TextField(verbose_name="内容")
-
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
