587017a40a12518ae3083141ae00beb24ec1f18e
hideame <hamezawa3@gmail.com>
2023-11-14 08:59:26 +0900

    投稿機能の追加

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from django.urls import path
 
 from lib.views import IndexTemplateView
-from recipe.views import RecipeListView
+from recipe.views import RecipeCreateView, RecipeListView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("recipe", RecipeListView.as_view(), name="recipe-index"),
+    path("recipe/create", RecipeCreateView.as_view(), name="recipe-create"),
     path("", IndexTemplateView.as_view(), name="index"),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,7 +1,13 @@
-from django.views.generic import ListView
+from django.views.generic import CreateView, ListView
 
 from .models import Recipe
 
 
 class RecipeListView(ListView):
     model = Recipe
+
+
+class RecipeCreateView(CreateView):
+    model = Recipe
+    fields = ["title", "content", "description"]
+    success_url = "/"
