c59e9737c2f0e008c0c2968c271878dc4d5d13c0
hideame <hamezawa3@gmail.com>
2023-11-13 14:54:21 +0900

    トップページ更新

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
+from lib.views import IndexTemplateView
 from recipe.views import RecipeListView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", RecipeListView.as_view(), name="index"),
+    path("recipe", RecipeListView.as_view(), name="recipe-index"),
+    path("", IndexTemplateView.as_view(), name="index"),
 ]
