749ea9f4af0a3c45d0210165f7189dfe08b07c4d
hideame <hamezawa3@gmail.com>
2023-11-15 09:57:19 +0900

    レシピの更新＆削除の実装

--- config/urls.py
+++ config/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from lib.views import IndexTemplateView
-from recipe.views import RecipeCreateView, RecipeListView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("recipe", RecipeListView.as_view(), name="recipe-index"),
-    path("recipe/create", RecipeCreateView.as_view(), name="recipe-create"),
+    path("recipe/", include("recipe.urls")),
     path("", IndexTemplateView.as_view(), name="index"),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,13 +1,58 @@
-from django.views.generic import CreateView, ListView
+from django.contrib import messages
+from django.urls import reverse, reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 
 from .models import Recipe
 
 
 class RecipeListView(ListView):
     model = Recipe
+    template_name = "recipe/recipe_list.html"
 
 
 class RecipeCreateView(CreateView):
     model = Recipe
+    template_name = "recipe/recipe_form.html"
     fields = ["title", "content", "description"]
-    success_url = "/"
+    success_url = reverse_lazy("recipe:index")
+
+    def form_valid(self, form):
+        messages.success(self.request, "保存しました")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "保存できませんでした")
+        return super().form_invalid(form)
+
+
+class RecipeDetailView(DetailView):
+    model = Recipe
+    template_name = "recipe/recipe_detail.html"
+
+
+class RecipeUpdateView(UpdateView):
+    model = Recipe
+    template_name = "recipe/recipe_form.html"
+    fields = ["title", "content", "description"]
+
+    def get_success_url(self):
+        pk = self.kwargs.get("pk")
+        return reverse("recipe:detail", kwargs={"pk": pk})
+
+    def form_valid(self, form):
+        messages.success(self.request, "更新しました")
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, "更新できませんでした")
+        return super().form_invalid(form)
+
+
+class RecipeDeleteView(DeleteView):
+    model = Recipe
+    template_name = "recipe/recipe_confirm_delete.html"
+    success_url = reverse_lazy("recipe:index")
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, "削除しました")
+        return super().delete(request, *args, **kwargs)
